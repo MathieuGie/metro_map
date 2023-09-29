0b46616bc6c1985f50e00b70bf1cfc0cb5e39d78
mathieu <gierski.mathieu@gmail.com>
2023-09-29 19:18:14 +0200

    density added

--- city.py
+++ city.py
@@ -44,20 +44,29 @@ class Metropolis:
         self.size=size
 
         self.area=[] #area of the whole metropolis (all the cities counted)
+        self.density = {}
+
         for city in self.cities:
             for pixel in city.area:
                 self.area.append(pixel)
+
+                if city == central_city:
+                    self.density[(pixel[0], pixel[1])] = (0.5, city)
+                else:
+                    self.density[(pixel[0], pixel[1])] = (0.3, city)
+
         
         self.frame=np.zeros((size,size)) #initialise the frame to empty map
-        
+
+
     def update_frame(self):
 
         frame=np.zeros((self.size,self.size))
         mid=int(self.size/2)
 
-        for pixel in self.area:
+        for pixel in self.density:
             #print(pixel)
-            frame[mid+pixel[0], mid+pixel[1]]=1
+            frame[mid+pixel[0], mid+pixel[1]]=self.density[pixel][0]
 
         self.frame=frame
 
@@ -75,7 +84,8 @@ class Metropolis:
         else:
             center=self.cities[index].center[0]
             d=np.sqrt(center[0]**2+center[1]**2)
-            p=((0.99999-p_others)/(np.sqrt(2)*250))*d+p_others
+            #p=((1-p_others)/(np.sqrt(2)*250))*d+p_others
+            p = p_others + p_others/(d*0.03)
 
         #Probabilistically grow the desired city
         for pixel in self.cities[index].area:
@@ -83,14 +93,53 @@ class Metropolis:
 
                 possible=[new[0]+pixel[0], new[1]+pixel[1]]
 
-                if random.uniform(0,1)>p:
+                if random.uniform(0,1)<p: #< instead of >
                     if (possible not in self.area) and (possible not in new_pixels):
                         if possible[0]<self.size/2 and possible[0]>-self.size/2 and possible[1]<self.size/2 and possible[1]>-self.size/2:
+                            
                             new_pixels.append(possible)
 
+                            
+
+        #Then harmonise all the pixels of thee city:
+        for pixel in self.cities[index].area:
+            for new in neighbors:
+                
+                possible=[new[0]+pixel[0], new[1]+pixel[1]]
+                possible_tuple = (new[0]+pixel[0], new[1]+pixel[1])
+                p_tilde=0
+                n=0
+
+                if possible in self.area:
+                    p_tilde+=self.density[possible_tuple][0]
+                    n+=1
+
+            if n!=0:
+                p_tilde/=n
+
+            p = self.density[(pixel[0], pixel[1])][0]
+            city = self.density[(pixel[0], pixel[1])][1]
+
+            if random.uniform(0,1)<p_tilde:
+                p += 0.01
+
+            if p>1:
+                p = 1
+
+            self.density[(pixel[0], pixel[1])] = (p, city)
+
+            
         for integrated in new_pixels:
+
             self.cities[index].area.append(integrated)
             self.area.append(integrated)
+
+            if index==0:
+                self.density[(integrated[0], integrated[1])] = (0.5, self.cities[index])
+            else:
+                dens = (-0.5/360)*euclidean(integrated, [0,0])+0.5
+                self.density[(integrated[0], integrated[1])] = (dens, self.cities[index])
+
 
         self.time+=1
 
@@ -143,22 +192,26 @@ class Metropolis:
         for city_index in range(len(self.cities)):
             self.grow(p_center, p_others, city_index)
 
-        proba=random.uniform(0,1)
-        if proba>p_new:
+        for _ in range(5): #Attempt to create at most 5 cities per round
+            proba=random.uniform(0,1)
+            if proba<p_new:
 
-            ok=False
-            while ok==False:
-                i=np.random.randint(-mid,mid)
-                j=np.random.randint(-mid,mid)
-                new_center=[i,j]
-                if new_center not in self.area:
-                    ok=True
-                    self.cities.append(City("hello"+str(self.time), self.time, [new_center]))
+                ok=False
+                while ok==False:
+                    i=np.random.randint(-mid,mid)
+                    j=np.random.randint(-mid,mid)
+                    new_center=[i,j]
+                    if new_center not in self.area:
+                        ok=True
+                        self.cities.append(City("hello"+str(self.time), self.time, [new_center]))
+                        dens = (-0.5/360)*euclidean(new_center, [0,0])+0.5
+                        self.density[(new_center[0], new_center[1])] = (dens, self.cities[-1])
 
         self.update_frame()
 
     def grow_station(self, station:Station, p_growth:float):
 
+        """
         neighbors=[[1,1],[1,0],[1,-1],[0,1],[0,-1],[-1,1],[-1,0],[-1,-1]]
 
         for neighb in neighbors:
@@ -170,9 +223,22 @@ class Metropolis:
                         self.area.append(possible)
 
         self.update_frame()
-
+        """
 
             
 
-#city=City("hello", 0, [[0,0]])
-#metropolis=Metropolis(city, [],0, 500)
+city=City("hello", 0, [[0,0]])
+metropolis=Metropolis(city, [],0, 500)
+
+print(np.sqrt(250**2+250**2))
+for i in range(25):
+    print("iteration:", i)
+    metropolis.new_round(0.4,0.1,0.7)
+
+print(metropolis.density[(0,0)])
+
+
+fig, ax = plt.subplots()
+ax.imshow(metropolis.frame)
+
+plt.show()
